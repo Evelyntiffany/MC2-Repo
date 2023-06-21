1c101130f8a6db80e6f8d5e3af874f92880c5134
Evelyn <evie7911@gmail.com>
2023-06-21 09:16:05 +0700

    Initial Commit

---
+++ MC2/MC2App.swift
@@ -0,0 +1,17 @@
+//
+//  MC2App.swift
+//  MC2
+//
+//  Created by Evelyn Megawati Tiffany on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
